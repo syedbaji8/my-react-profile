e3d5416e51b970c282762f54fd310c81a1fd2b75
Shaik Baji <syedbaji8@gmail.com>
2025-10-04 15:40:49 +0530

    fixes updated

--- src/components/Navigationdetails.jsx
+++ src/components/Navigationdetails.jsx
@@ -7,7 +7,7 @@ export default function Navigationdetails({ gotothePage, activeSection }) {
             {
                 navarray.map((navname) =>
                     <button
-                        className={`bg-[#444]-100 border rounded-full px-4 py-2 whitespace-nowrap text-sm mb-14 cursor-pointer shadow-[2px_4px_0px_black] ${activeSection === navname ? "bg-gray-200 text-black" : "bg-gray-800 text-white" }`}
+                        className={`border rounded-full px-4 py-2 whitespace-nowrap text-sm mb-14 cursor-pointer shadow-[2px_4px_0px_black] ${activeSection === navname ? "bg-[#222] text-white" : "bg-[#444]-100 text-white" }`}
                         onClick={() => gotothePage(navname)}>
                         {navname}
                     </button>)
